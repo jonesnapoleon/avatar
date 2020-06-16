86f7cec62301e6a78ec131e4274a97bc9acf1912
Jones Napoleon <jonesnapoleonautumn1@gmail.com>
2020-06-16 23:24:32 +0700

    Updates

---
+++ precache-manifest.a533ec84ede853738a9612b621a88554.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "691f6945373918c70c927faab0b0819e",
+    "url": "/avatar/index.html"
+  },
+  {
+    "revision": "ab167a99dc557734273e",
+    "url": "/avatar/static/css/main.87d3e42a.chunk.css"
+  },
+  {
+    "revision": "48ff13badec1a38bbcdf",
+    "url": "/avatar/static/js/2.35804151.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/avatar/static/js/2.35804151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab167a99dc557734273e",
+    "url": "/avatar/static/js/main.ec03cba8.chunk.js"
+  },
+  {
+    "revision": "5201ada2612a8108f9c5",
+    "url": "/avatar/static/js/runtime-main.ca3d9a45.js"
+  }
+]);
