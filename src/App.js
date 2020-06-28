99608f8a5aad67611d25d56d070974409ea4e36f
Jones Napoleon <jonesnapoleonautumn1@gmail.com>
2020-06-28 13:40:23 +0700

    change some

--- src/App.js
+++ src/App.js
@@ -13,21 +13,17 @@ export default () => {
       links: localStorage.getItem('links') ? JSON.parse(localStorage.getItem('links')) : []
     }
   )
-
   const handleData = (inputNode) => {
     let temp = data
-
     temp.nodes = Array.from(new Set([...temp.nodes, { id: inputNode.id, name: inputNode.name, element: inputNode.element }, ...inputNode.friends]))
     temp.nodes = temp.nodes.filter((node, i) => i === temp.nodes.findIndex(newNode => node.id === newNode.id))
     temp.nodes.map(node => node.color = graphNodeColor[node.element])
     temp.nodes.map(node => node.highlightStrokeColor = graphNodeColor[node.element])
     temp.nodes.map(node => node.fontColor = graphNodeColor[node.element])
-
     let inputToFriendNode = inputNode.friends.map(friendNode => friendNode.id !== inputNode.id ? { source: inputNode.id, target: friendNode.id } : null)
     let friendToInputNode = inputNode.friends.map(friendNode => friendNode.id !== inputNode.id ? { source: friendNode.id, target: inputNode.id } : null)
     temp.links = Array.from(new Set([...temp.links, ...inputToFriendNode, ...friendToInputNode])).filter(x => x !== null)
     temp.links = temp.links.filter((link, i) => i === temp.links.findIndex(newLink => link.source === newLink.source && link.target === newLink.target))
-
     localStorage.setItem('nodes', JSON.stringify(temp.nodes))
     localStorage.setItem('links', JSON.stringify(temp.links))
     setData(temp)
@@ -41,10 +37,9 @@ export default () => {
     handleData(data.payload)
     setFetchedId([...fetchedId, data.payload.id])
   }
-
   const handleSubmit = async (e) => e.key === 'Enter' && await handleIndex(e.target.value.trim())
-
   const handleDelete = () => {
+    setFetchedId([])
     setData({ nodes: [], links: [] })
     localStorage.removeItem("nodes")
     localStorage.removeItem('links')
